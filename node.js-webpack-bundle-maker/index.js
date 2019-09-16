5d7f9926c93f61298060d1c10dbf21bdcd77c95e
J.E. R <je-r@users.noreply.github.com>
2019-09-16 09:29:36 +0200

    dev

--- node.js-webpack-bundle-maker/index.js
+++ node.js-webpack-bundle-maker/index.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const webpack = require('webpack');
 const webpackMiddleware = require('webpack-dev-middleware');
+
+const config = require('./webpack.config.js');
+const compiler = webpack(config);
+
 // TODO complete
+
