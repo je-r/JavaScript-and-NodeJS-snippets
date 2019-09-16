feac2f7632ce3f65db2f933b34bc1803b607db8a
J.E. R <je-r@users.noreply.github.com>
2019-09-16 18:37:03 +0200

    dev

--- node.js-webpack-bundle-maker/webpack.config.js
+++ node.js-webpack-bundle-maker/webpack.config.js
@@ -1,5 +1,11 @@
 // TODO 
 
-module.exports = {
-// TODO
-};
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  }
+}
+
+module.exports = config;
