d70428c5ddea5f0a04442cc780651d38b6bc0202
J.E. R <je-r@users.noreply.github.com>
2019-09-19 07:35:41 +0200

    dev

---
+++ node.js-webpack-from-source-in-cloud/index.js
@@ -0,0 +1,20 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = require('./webpack.config.js');
+const compiler = webpack(config);
+
+//infos about running webpack compiler: https://webpack.js.org/api/node/
+
+compiler.run((err, stats) => { // Stats Object
+  // ...
+  console.log("running webpack compiler...");
+  if (err || stats.hasErrors()) {
+    // Handle errors here
+    console.log("error occured");
+    console.log("err="+err);
+    console.log("stats="+stats);
+  }
+  console.log("done processing");
+});
+
---
+++ node.js-webpack-from-source-in-cloud/webpack.config.js
@@ -0,0 +1,16 @@
+const config = {
+  mode: 'development',
+  
+  entry: {
+    'bundle.js': [
+      path.resolve(__dirname, './test.js'),
+      path.resolve(__dirname, './other.js')
+    ]
+  },
+  output: {
+    filename: '[name]',
+    path: path.resolve(__dirname, 'dist'),
+  }
+}
+
+module.exports = config;
