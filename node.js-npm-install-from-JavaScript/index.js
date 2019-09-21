a5d31e3c43224a35cd7021e971a2ab90a6326718
J.E. R <je-r@users.noreply.github.com>
2019-09-21 17:02:02 +0200

    dev

--- node.js-npm-install-from-JavaScript/index.js
+++ node.js-npm-install-from-JavaScript/index.js
@@ -1,10 +1,29 @@
 // Node.js-npm-install-from-JavaScript
 
-const npm = require('npm');
+var npm = require('npm');
+npm.load(function(err) {
+  // handle errors
+  console.log("npm loaded");
+  console.log("err="+err);
 
+  // install module ffi
+  npm.commands.install(['ffi'], function(er, data) {
+    // log errors or data
+    console.error(er);
+  });
+  
+
+  npm.on('log', function(message) {
+    // log installation progress
+    console.log(message);
+  });
+});
+
+/*
 try {
   console.log("type="+ typeof npm.command );
 }
 catch(error) {
   console.error(error);
 }
+*/
