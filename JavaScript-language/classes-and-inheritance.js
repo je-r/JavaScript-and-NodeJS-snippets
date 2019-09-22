25115407e8f68cafedd7626beb76d052e4f83477
J.E. R <je-r@users.noreply.github.com>
2019-09-22 16:28:13 +0200

    dev

--- JavaScript-language/classes-and-inheritance.js
+++ JavaScript-language/classes-and-inheritance.js
@@ -6,7 +6,41 @@ class Cheese
     {
         console.log("Cheese class: in constructor");
     }
+    
+    eat()
+    {
+        console.log("Cheese class: in eat()");
+    }
 }
 
-var Scamorza = new Cheese();
+var Scamorza = new Cheese(); // runs code in constructor
+
+class Food {
+    constructor()
+    {
+        console.log("Food class: in constructor");
+    }
+    
+    eat()
+    {
+        console.log("Food class: in eat()");
+    }
+}
+
+class BetterCheese extends Food
+{
+    constructor()
+    {
+        super();
+    }
+    
+    eat()
+    {
+        console.log("BetterCheese class: in eat()");
+    }
+}
+
+var Esrom = new BetterCheese(); // runs code in constructor
+
+
 
