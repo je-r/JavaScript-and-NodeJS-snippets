c9c8a4a3b91021d3eceb155374113a75a61b8c12
J.E. R <je-r@users.noreply.github.com>
2019-09-26 07:15:57 +0200

    dev

--- node.js-gulp-concatenate/gulpfile.js
+++ node.js-gulp-concatenate/gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 
-gulp.task('default', [], function() {
+gulp.task('default', function() {
   console.log("Concating and moving all the js files in src folder");
   gulp.src("src/**.js")
     .pipe(concat('main.js'))
