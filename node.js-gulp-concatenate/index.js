4b6edff102e65fb53d03139640d26d5c34ca3926
J.E. R <je-r@users.noreply.github.com>
2019-09-26 07:09:26 +0200

    dev

--- node.js-gulp-concatenate/index.js
+++ node.js-gulp-concatenate/index.js
@@ -1,2 +1,9 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var gulp = require('gulp');
+
+require('./gulpfile');
+
+// calling "gulp default" programmatically
+
+gulp.series("default")();
