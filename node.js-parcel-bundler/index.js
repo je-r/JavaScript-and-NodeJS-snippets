943377c212aa12d87219e9ce7f3dbadd3c2f1a1c
J.E. R <je-r@users.noreply.github.com>
2019-10-01 07:14:23 +0200

    dev

--- node.js-parcel-bundler/index.js
+++ node.js-parcel-bundler/index.js
@@ -3,4 +3,19 @@ const Path = require('path');
 
 const entryFiles = './src/*.js';
 
-// TODO complete 
+// Bundler options
+const options = {
+  outDir: './dist'
+};
+
+
+(async function() {
+  // Initializes a bundler using the entrypoint location and options provided
+  const bundler = new Bundler(entryFiles, options);
+
+  // Run the bundler, this returns the main bundle
+  // Use the events if you're using watch mode as this promise will only trigger once and not for every rebuild
+  const bundle = await bundler.bundle();
+})();
+
+// TODO complete and test!
