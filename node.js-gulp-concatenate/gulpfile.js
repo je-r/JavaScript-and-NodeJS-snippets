c2ea2a20a881a0868f8b4be74b12551b5f74a9d7
rutkowski <5433476+je-r@users.noreply.github.com>
2019-10-20 18:30:28 +0200

    dev

--- node.js-gulp-concatenate/gulpfile.js
+++ node.js-gulp-concatenate/gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var rimraf = require('rimraf');
 
-gulp.task('default', function() {
+gulp.task('default', function(done) {
   console.log("Concating and moving all the js files in src folder");
   gulp.src("src/**.js")
     .pipe(concat('main.js'))
     .pipe(gulp.dest('build/js'));
+  done();
+});
+
+gulp.task("clean", function (done) {
+  rimraf("./build", done);
+  done();
 });
