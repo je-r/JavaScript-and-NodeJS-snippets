ce4e0e43f6e74050917b3e681742ebfc62aace14
J.E. R <je-r@users.noreply.github.com>
2020-02-26 12:19:46 +0100

    dev

---
+++ node.js-npm-run-from-JavaScript/index.js
@@ -0,0 +1,37 @@
+// Node.js-npm-install-from-JavaScript
+
+var npm = require('npm');
+npm.load(function(err) {
+  // handle errors
+  console.log("npm loaded");
+  console.log("err="+err);
+
+console.log("npm.cache ="+ npm.cache );
+
+npm.cache = __dirname+"/.npm";
+
+console.log("npm.cache ="+ npm.cache );
+
+
+  // run build 
+  npm.commands.run(['build'], function(er, data) {
+    // log errors or data
+    console.error(er);
+  });
+  
+  
+
+  npm.on('log', function(message) {
+    // log installation progress
+    console.log(message);
+  });
+});
+
+/*
+try {
+  console.log("type="+ typeof npm.command );
+}
+catch(error) {
+  console.error(error);
+}
+*/
