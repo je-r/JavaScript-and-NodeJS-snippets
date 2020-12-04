e7803788e27e06a8b92cad1f85791e16793513e1
J.E. R <5433476+je-r@users.noreply.github.com>
2020-12-04 17:23:40 +0100

    Update client-side-Client-Object-Model-javascript-console-only.js

--- SharePoint-online/client-side-Client-Object-Model-javascript-console-only.js
+++ SharePoint-online/client-side-Client-Object-Model-javascript-console-only.js
@@ -1,3 +1,4 @@
+// step 1
 
 function loadjscssfile(filename, filetype){
     if (filetype=="js"){ //if filename is a external JavaScript file
@@ -15,10 +16,55 @@ function loadjscssfile(filename, filetype){
         document.getElementsByTagName("head")[0].appendChild(fileref)
 }
 
+// step 2
 
 loadjscssfile("_layouts/1033/init.js","js");
 loadjscssfile("_layouts/15/MicrosoftAjax.js","js");
+
+// step 3
+
 loadjscssfile("_layouts/sp.core.js","js");
 loadjscssfile("_layouts/sp.runtime.js","js");
 loadjscssfile("_layouts/sp.js","js");
-var ctx = new SP.ClientContext("/");
+
+// step 4
+
+var siteUrl = "/sites/someExistingSite";
+var ctx = new SP.ClientContext(siteUrl);
+
+// step 4
+
+
+function onQuerySucceeded() {
+
+    var listInfo = '';
+
+    var listEnumerator = collList.getEnumerator();
+
+    while (listEnumerator.moveNext()) {
+        var oList = listEnumerator.get_current();
+        listInfo += 'Title: ' + oList.get_title() + ' Created: ' + oList.get_created().toString() + '\n';
+    }
+    console.log(listInfo);
+}
+
+function onQueryFailed(sender, args) {
+    console.log('Request failed. ' + args.get_message() + '\n' + args.get_stackTrace());
+}
+
+function retrieveAllListProperties() {
+
+    var clientContext = new SP.ClientContext(siteUrl);
+    var oWebsite = clientContext.get_web();
+    this.collList = oWebsite.get_lists();
+ 
+    clientContext.load(collList);
+
+    clientContext.executeQueryAsync(Function.createDelegate(this, this.onQuerySucceeded), Function.createDelegate(this, this.onQueryFailed));
+}
+
+
+// step 4
+
+retrieveAllListProperties();
+
